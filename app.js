8bcd10b3b0c1037aaac3bcb2776d39f438d95f35
Keyu Lin <mlchaellin222@gmail.com>
2017-12-21 16:14:30 -0500

    finished bonus: enabling local saving whenever there is insertion,deletion or checking

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 function onReady() {
   let id=0;
   let toDos = [];
+  if(localStorage.getItem("arr")){
+    var toDos_str=localStorage.getItem("arr");
+    toDos=JSON.parse(toDos_str);
+  }
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
@@ -15,10 +19,15 @@ function onReady() {
     });
     newToDoText.value = '';
     id+=1;
+    localStorage.setItem('arr', JSON.stringify(toDos));
     renderTheUI();
   }
   function deleteToDo(id){
     toDos = toDos.filter(item => item.id !== id);
+    localStorage.setItem('arr', JSON.stringify(toDos));
+  }
+  function checkboxchanged() {
+    alert("changed");
   }
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
@@ -30,7 +39,31 @@ function onReady() {
 
        const checkbox = document.createElement('input');
        checkbox.type = "checkbox";
+       //add checked if checked
+       checkbox.checked=toDo.complete;
+       //checkbox.onclick="checkboxchanged()";
+       checkbox.addEventListener("click", function(){
 
+         var flag=0;
+         if(checkbox.checked){
+           flag=1;
+         }
+         for(var i=0;i<toDos.length;i++){
+           if(toDos[i].title==checkbox.parentNode.firstChild.textContent){
+             if(flag==1){
+               toDos[i].complete=true;
+             }else{
+               toDos[i].complete=false;
+             }
+             break;
+           }
+         }
+
+          //test
+          //alert(checkbox.parentNode.firstChild.textContent+JSON.stringify(toDos));
+         //save
+         localStorage.setItem('arr', JSON.stringify(toDos));
+       });
        const title = document.createElement('span');
        title.textContent = toDo.title;
 
